59248975b3bdafe36ea73104e068ef3026d2c5fc
Ana Cecília <acsf6@aluno.ifnmg.edu.br>
2023-11-22 20:56:11 -0300

    ss

---
+++ index.js
@@ -0,0 +1,27 @@
+import { selectUsuarios, selectUsuario, insertUsuario,  deleteUsuario, updateUsuario } from "./db/index.js";
+import dotenv from "dotenv";
+import express from "express";  
+import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
+
+dotenv.config();
+
+const app = express();              
+const port = 3000;                  
+
+app.use(express.json());
+app.use(roteadorUsuario);
+app.use(roteadorLogin);
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {       
+  res.json({
+    nome: "Venda de Livros",     
+  });
+  console.log("Rota / solicitada");
+});
+
+app.listen(port, () => {          
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
