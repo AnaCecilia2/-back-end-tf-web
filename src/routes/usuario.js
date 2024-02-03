ebf2d33483f5e8b7ed9bb9bc8f3f3913172df5cf
AnaCecilia2 <132697963+AnaCecilia2@users.noreply.github.com>
2024-02-03 00:41:39 -0300

    Update usuario.js

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -80,7 +80,7 @@ router.post("/usuario", async (req, res) => {
 router.put("/usuario", verificarAutenticacao,  async (req, res) => {
   console.log("Rota PUT /usuario solicitada");
   try {
-    const usuario = await selectUsuario(req.body.nome);
+    const usuario = await selectUsuario(req.body.idusuario);
     if (usuario.length > 0) {
       await updateUsuario(req.body);
       res.status(200).json({ message: "Usuário atualizado com sucesso!" });
